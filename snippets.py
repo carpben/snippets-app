d6d828de05113aad61e59460d827912a5b294147
carpben <bccarp@gmail.com>
2015-09-07 15:27:09 +0000

    Add parser structure

--- snippets.py
+++ snippets.py
@@ -37,12 +37,29 @@ def main():
     put_parser.add_argument("name", help="The name of the snippet")
     put_parser.add_argument("snippet", help="The snippet text")
 
-    arguments = parser.parse_args(sys.argv[1:])
-    arguments2 = parser.parse_args()
-    print ('-'*20)
-    print (arguments)
-    print ('-'*20)
-    print (arguments2)
+    # Subparser for the get command
+    get_parser = subparsers.add_parser("get", help="Retrieve a snippet")
+    get_parser.add_argument("name", help="The name of the snippet")
+    
+    logging.debug('{}\nparser.parse_args():\n{}'.format('-'*20, parser.parse_args()))
+    
+    arguments = parser.parse_args()
+    # Convert parsed arguments from Namespace to dictionary
+    
+    logging.debug('{}\narguments.__dict__\n{}'.format('-'*20, arguments.__dict__))
+    logging.debug('{}\nlocals()\n{}'.format('-'*20, locals()))
+    logging.debug('{}\nvars(arguments))\n{}'.format('-'*20, vars(arguments)))
+    
+    arguments = vars(arguments)
+    command = arguments.pop("command")
 
+    if command == "put":
+        name, snippet = put(**arguments)
+        print("Stored {!r} as {!r}".format(snippet, name))
+    elif command == "get":
+        snippet = get(**arguments)
+        print("Retrieved snippet: {!r}".format(snippet))
+    
+    
 if __name__ == "__main__":
     main()
